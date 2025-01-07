01b0da9d6bb5a36018ebcefb16b2d7152e3648e4
Nuruddin Syed <nuruddinsyed18@outlook.com>
2025-01-07 23:56:07 +0530

    Automation Assignment

---
+++ src/com/java/assignments/HelloJava.java
@@ -0,0 +1,10 @@
+package com.java.assignments;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("HelloJava");
+	}
+
+}
